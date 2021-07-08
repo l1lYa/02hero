bad6e6e2dc29df4506597759f86232e83b0eecf9
Ilya Sokolov <ilya.sokolov@amvonet.com>
2021-07-08 16:41:21 +0400

    class Variables list of primitive variables

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String [] args){
+        int myInt = 741;
+        short myShort = 7;
+        long myLong = 19234120;
+
+        double myDouble = 741.22;
+        float myFloat = 741.741741f;
+
+        char a = 'i';
+        boolean b = true;
+
+        byte myByte = 77; //-128 127
+        System.out.println(myInt);
+    }
+}
