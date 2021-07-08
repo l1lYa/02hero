5d0b512a0e17c9813db6666f0682c0d92fb15004
Ilya Sokolov <ilya.sokolov@amvonet.com>
2021-07-08 17:46:24 +0400

    class Strings diffrent Strings out

---
+++ src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String [] args){
+        int z = 7;
+        String a = "Hello";
+        String b = " ";
+        String c = "Maxim";
+        System.out.println(a+b+c);
+        System.out.println("Hello" + " " + "Ilya");
+        System.out.println("My number is = " + z);
+        System.out.println("My number is = " + 7);
+    }
+}
