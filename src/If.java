36addf476294c9b1d102062f992a7c88e5ef6a00
Ilya Sokolov <ilya.sokolov@amvonet.com>
2021-07-08 18:40:26 +0400

    class If

---
+++ src/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String [] args){
+        int myInt = 15;
+        if(myInt<10){
+            System.out.println("Верно");
+        } else if(myInt>20){
+            System.out.println("Не верно");
+        } else{
+            System.out.println("Не угадал");
+        }
+    }
+}
