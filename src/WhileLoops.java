de2c80b7b6d76476ffa6c44deae7c26b7a330630
Ilya Sokolov <ilya.sokolov@amvonet.com>
2021-07-08 18:05:56 +0400

    class WhileLoops

---
+++ src/WhileLoops.java
@@ -0,0 +1,9 @@
+public class WhileLoops {
+    public static void main(String [] args){
+        int value = 1;
+        while (value < 6){
+            System.out.println("Hello " + value);
+            value +=1;
+        }
+    }
+}
